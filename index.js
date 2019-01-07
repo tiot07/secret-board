7bb7c203c35c98c89763ec56715d2d65a1d2b31a
tiot07 <tiot7155@gmail.com>
2019-01-07 13:11:30 +0900

    index実装

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
 const http = require('http');
+const router = require('./lib/router');
 
-const server = http.createServer((req,res)=>{
-  res.end('hi');
-}).on('error',(e)=>{
-  console.error('Server Error',e);
-}).on('clientError',(e)=>{
-  console.error('Client Error',e);
+const server = http.createServer((req, res) => {
+  router.route(req, res);
+}).on('error', (e) => {
+  console.error('Server Error', e);
+}).on('clientError', (e) => {
+  console.error('Client Error', e);
 });
 
-const port=8000;
-server.listen(port,()=>{
-  console.info('Listening on '+port);
+const port = 8000;
+server.listen(port, () => {
+  console.info('Listening on ' + port);
 });
